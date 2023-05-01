917ec49cccf33309b62b1a34805ebe97eaa1c34e
dann <dannforsb@gmail.com>
2023-05-01 12:56:03 +0330

    init some begining files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, BookModule],
 })
 export class AppModule {}
