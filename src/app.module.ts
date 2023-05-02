cb3cce53b28b94a8b5d5691bf6addcba410468c1
dann <dannforsb@gmail.com>
2023-05-02 12:28:53 +0330

    add pipes in main.ts and config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+// modules
 import { UserModule } from './user/user.module';
 import { BookModule } from './book/book.module';
 
+// entities
+import { User } from './user/entities/user.entity';
+
 @Module({
-  imports: [UserModule, BookModule],
+  imports: [
+    UserModule,
+    BookModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'bookstore',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
