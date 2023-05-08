fe9f3fab2f6d890c23f714611a8ca509f065c713
dann <dannforsb@gmail.com>
2023-05-08 20:53:22 +0330

    add book init service

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BookService {
-    
+  constructor(@InjectRepository(Book) private repo: Repository<Book>) {}
+
+  create(name: string, code: string, category: string) {
+    const book = this.repo.create({ name, code, category });
+    return this.repo.save(book);
+  }
+
+  findOne(code: string) {
+    return this.repo.findOne({
+      where: {
+        code,
+      },
+    });
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
 }
